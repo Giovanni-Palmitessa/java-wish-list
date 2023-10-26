8bf7888ebbcf02cc6b625754c5f99dbcfdffe21a
Giovanni-Palmitessa <giovicchio@gmail.com>
2023-10-26 14:43:17 +0200

    Add getter and setter

--- src/org/lessons/java/Gift.java
+++ src/org/lessons/java/Gift.java
@@ -10,4 +10,18 @@ public class Gift {
         this.description = description;
         this.recipient = recipient;
     }
+
+    //Getter e Setter
+    public String getDescription() {
+        return description;
+    }
+    public void setDescription(String description) {
+        this.description = description;
+    }
+    public String getRecipient() {
+        return recipient;
+    }
+    public void setRecipient(String recipient) {
+        this.recipient = recipient;
+    }
 }

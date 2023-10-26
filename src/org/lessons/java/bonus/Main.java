6b01b8d6abe1ee93887a2b9db8185fd96a14cafc
Giovanni-Palmitessa <giovicchio@gmail.com>
2023-10-26 15:49:59 +0200

    start bonus exercise

---
+++ src/org/lessons/java/bonus/Main.java
@@ -0,0 +1,13 @@
+package org.lessons.java.bonus;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        // istanzio lo scanner
+        Scanner scan = new Scanner(System.in);
+
+        // chiedo all'utente di darmi la parola
+        System.out.println("Dammi una parola e ti dirò le lettere.");
+    }
+}

48bf033eaa2abe3bc741a154dbf7b9f9d05bfcd7
Giovanni-Palmitessa <giovicchio@gmail.com>
2023-10-26 14:45:23 +0200

    Add new gifts collection

--- src/org/lessons/java/Main.java
+++ src/org/lessons/java/Main.java
@@ -1,10 +1,15 @@
 package org.lessons.java;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
         // istanzio lo scanner
         Scanner scan = new Scanner(System.in);
+
+        //creo una collection di Regali
+        List<Gift> gifts = new ArrayList<>();
     }
 }

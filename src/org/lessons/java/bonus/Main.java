7a0f47bcec09bb985aaaaec6d54e3250e2913974
Giovanni-Palmitessa <giovicchio@gmail.com>
2023-10-26 18:00:13 +0200

    add bonus

--- src/org/lessons/java/bonus/Main.java
+++ src/org/lessons/java/bonus/Main.java
@@ -4,6 +4,7 @@ import java.util.HashMap;
 import java.util.Map;
 import java.util.Scanner;
 
+
 public class Main {
     public static void main(String[] args) {
         // istanzio lo scanner
@@ -17,6 +18,24 @@ public class Main {
         Map<Character, Integer> occorrenze = new HashMap<>();
 
         // Iterare attraverso i caratteri della parola e conta le occorrenze
+        for (char carattere : word.toCharArray()) {
+            // Verifica se il carattere è già presente nella mappa
+            if (occorrenze.containsKey(carattere)) {
+                // Se sì, incrementa il conteggio
+                occorrenze.put(carattere, occorrenze.get(carattere) + 1);
+            } else {
+                // Altrimenti, aggiungi il carattere alla mappa con un conteggio di 1
+                occorrenze.put(carattere, 1);
+            }
+        }
+
+        // Stampa le occorrenze dei caratteri
+        for (char carattere : occorrenze.keySet()) {
+            int conteggio = occorrenze.get(carattere);
+            System.out.println(carattere + ":" + conteggio);
+        }
+
+        scan.close();
 
     }
 }
